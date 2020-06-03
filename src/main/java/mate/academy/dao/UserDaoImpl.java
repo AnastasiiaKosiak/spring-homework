6467051ccf8a9dc276b6f1cc4c05196ebdaabe9a
nastya <cctvporsh@gmail.com>
2020-06-03 11:57:25 +0300

    refactore UserDaoImpl

--- src/main/java/mate/academy/dao/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/UserDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.dao;
 
 import java.util.List;
 import javax.persistence.TypedQuery;
+import mate.academy.exceptions.DataProcessingException;
 import mate.academy.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,12 +21,32 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void add(User user) {
-        sessionFactory.openSession().save(user);
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception exception) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert a User entity", exception);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.openSession().createQuery("from User", User.class);
-        return query.getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            TypedQuery<User> query = session.createQuery("from User", User.class);
+            return query.getResultList();
+        } catch (Exception exception) {
+            throw new DataProcessingException("Error getting a list of all movies", exception);
+        }
     }
 }
